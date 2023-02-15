b0ed28a7eea7a7e4cceeb3c056cdb43aa809dbda
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-02-16 00:33:27 +0800

    Update leetcode.cpp

--- c/leetcode.cpp
+++ c/leetcode.cpp
@@ -48,6 +48,67 @@ public:
     }
 };
 
+// No.73
+// space O(1)
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        bool firstr = false;
+        bool firstc = false;
+        for(int j = 0; j < n; j++)
+        {
+            if(matrix[0][j]==0)
+            {
+                firstr = true;
+                break;
+            }   
+        }
+        for(int i = 0; i < m; i++)
+        {
+            if(matrix[i][0]==0)
+            {
+                firstc = true;
+                break;
+            }
+        }
+
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                if(matrix[0][j]==0 || matrix[i][0]==0)
+                    matrix[i][j] = 0;
+            }
+        }
+
+        if(firstr)
+        {
+            for(int j = 0; j < n; j++)
+                matrix[0][j] = 0;
+        }
+        if(firstc)
+        {
+            for(int i = 0; i < m; i++)
+                matrix[i][0] = 0;
+        }
+    }
+};
+
 // No.94
 /**
  * Definition for a binary tree node.
