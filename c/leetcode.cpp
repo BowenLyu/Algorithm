d83f3385290ce40526993563feb5f97646030ba0
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-02-15 15:38:39 +0800

    Update leetcode.cpp

--- c/leetcode.cpp
+++ c/leetcode.cpp
@@ -28,3 +28,46 @@ public:
         
     }
 };
+
+
+//No.102
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode*> level1;
+        queue<TreeNode*> level2;
+        vector<vector<int>> level_list;
+        if(root != NULL)
+        {
+            level1.push(root);
+            do{
+                vector<int> level1_val;
+                TreeNode* current;
+                while(! level1.empty()){
+                current = level1.front();
+                level1.pop();
+                if(current->left != NULL)
+                    level2.push(current->left);
+                if(current->right != NULL)
+                    level2.push(current->right);
+                level1_val.push_back(current->val);
+                }
+                level_list.push_back(level1_val);
+                level1 = level2;
+                level2 = queue<TreeNode*>();
+            }while(!level1.empty());    
+        }
+        return level_list;
+    }
+};
