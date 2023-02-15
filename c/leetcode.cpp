370ccd3bc3796c636e1ef959ca35ef24aa7a38e0
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-02-15 16:36:09 +0800

    Update leetcode.cpp

--- c/leetcode.cpp
+++ c/leetcode.cpp
@@ -71,3 +71,47 @@ public:
         return level_list;
     }
 };
+
+// No.110
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL)
+            return true;
+        if(abs(heightoftree(root->left) - heightoftree(root->right)) > 1)
+            return false;
+        
+        if(! isBalanced(root->left))
+            return false;
+        if(! isBalanced(root->right))
+            return false;
+
+        return true;
+    }
+
+    int heightoftree(TreeNode* root){
+        if(root == NULL)
+            return 0;
+        int height = 0;
+        int height1 = heightoftree(root->left);
+        int height2 = heightoftree(root->right);
+
+        if(height1 > height2)
+            height = height1;
+        else
+            height = height2;
+            
+        return height + 1;
+    }
+};
