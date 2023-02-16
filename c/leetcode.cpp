73a18bdc941af9ae3cb901816f80a396386efb94
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-02-16 15:01:22 +0800

    Update leetcode.cpp

--- c/leetcode.cpp
+++ c/leetcode.cpp
@@ -53,6 +53,75 @@ public:
     }
 };
 
+// No.23
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int num = lists.size();
+        if(num == 0)
+            return NULL;
+
+        while(num > 1){
+            int j = 0;
+            for(int i = 0; i < num; i+=2)
+            {
+                if( i+1 >= num)
+                    lists[j] = merge2Lists(lists[i], nullptr);
+                lists[j] = merge2Lists(lists[i], lists[i+1]);
+                j++;
+            }
+
+            num = num/2 + num%2;
+        }
+            
+        return lists[0];
+        
+    }
+
+    ListNode* merge2Lists(ListNode* list1, ListNode* list2){
+        ListNode* merge;
+
+        if(list1 == NULL)
+            return list2;
+        if(list2 == NULL)
+            return list1;
+        
+        ListNode* temp = merge;
+
+        while(list1 && list2)
+        {
+            if(list1->val <= list2->val)
+            {
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            else
+            {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
+        }
+        
+        if(list1)
+            temp->next = list1;
+        if(list2)
+            temp->next = list2;
+
+        return merge->next;
+    }
+};
+
 // No.54
 class Solution {
 public:
