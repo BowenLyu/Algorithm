962ef337975aa5325eebd80bb5401c288b36f463
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-02-21 12:21:02 +0800

    Update leetcode.cpp

--- c/leetcode.cpp
+++ c/leetcode.cpp
@@ -331,6 +331,23 @@ public:
     }
 };
 
+// No.12
+class Solution {
+public:
+    string intToRoman(int num) {
+        string Roman;
+        pair<int,string> n2r[] = {{1000,"M"},{900,"CM"},{500,"D"},{400,"CD"},{100,"C"},{90,"XC"},{50,"L"},{40,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}};
+        for(auto& [n, s]: n2r){
+            while(num >= n){
+                Roman.append(s);
+                num -= n;
+            }
+        }
+
+        return Roman;
+    }
+};
+
 // No.13
 class Solution {
 public:
