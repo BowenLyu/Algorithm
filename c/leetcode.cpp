244ead14ca972134ac1a3d137eb5690df91a670b
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-02-25 21:57:41 +0800

    Update leetcode.cpp

--- c/leetcode.cpp
+++ c/leetcode.cpp
@@ -539,6 +539,40 @@ public:
     }
 };
 
+// No.19
+// 注意使用哑节点
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* cut = dummy;
+        ListNode* cur = head;
+
+        for(int i = 0; i < n; i++)
+            cur = cur->next;
+
+        while(cur){
+            cur = cur->next;
+            cut = cut->next;
+        }
+        cut->next = (cut->next)->next;
+        ListNode* ans = dummy->next;
+        delete dummy;
+
+        return ans;
+    }
+};
+
 // No.21
 /**
  * Definition for singly-linked list.
