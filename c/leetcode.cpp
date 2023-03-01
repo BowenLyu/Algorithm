fb253248c35a0d6ba22a620b777ad2ef06418751
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-03-01 11:19:36 +0800

    Update leetcode.cpp

--- c/leetcode.cpp
+++ c/leetcode.cpp
@@ -1149,6 +1149,38 @@ public:
     }
 };
 
+// No.36
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int row[9][9];
+        int col[9][9];
+        int subbox[3][3][9];
+
+        memset(row, 0, sizeof(row));
+        memset(col, 0, sizeof(col));
+        memset(subbox, 0, sizeof(subbox));
+        
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j< 9; j++){
+                if(board[i][j] == '.')
+                    continue;
+                int num = board[i][j] - '0' - 1;
+                if(row[i][num])
+                    return false;
+                if(col[j][num])
+                    return false;
+                if(subbox[i/3][j/3][num])
+                    return false;
+                row[i][num] = 1;
+                col[j][num] = 1;
+                subbox[i/3][j/3][num] = 1;
+            }
+        }
+        return true;
+    }
+};
+
 // No.53
 class Solution {
 public:
