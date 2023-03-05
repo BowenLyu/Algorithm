3a98512c40af0976ea9e4f48e509422da92c7488
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-03-05 23:13:12 +0800

    Update and rename offer.c to offer.cpp

--- c/offer.cpp
+++ c/offer.cpp
@@ -244,3 +244,24 @@ public:
         return a2;
     }
 };
+
+//11
+//重点温习的题目
+class Solution {
+public:
+    int minArray(vector<int>& numbers) {
+        int l = 0;
+        int r = numbers.size() -1;
+        int mid;
+        while(l < r){
+            mid = (l + r)/2;
+            if(numbers[mid] < numbers[r])
+                r = mid;
+            else if(numbers[mid] > numbers[r])
+                l = mid + 1;
+            else
+                r--;
+        }       
+        return numbers[r];
+    }
+};
