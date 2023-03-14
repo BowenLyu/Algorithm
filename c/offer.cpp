47cc8dd638cdae93ee2905a1b6ba7c2699e5b05e
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-03-14 21:34:47 +0800

    Update offer.cpp

--- c/offer.cpp
+++ c/offer.cpp
@@ -742,3 +742,46 @@ public:
         return output;
     }
 };
+
+// 30 
+// 最小栈数据结构，不是很熟，不难需要回顾一下
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    stack<int> s;
+    stack<int> mins;
+
+    MinStack() {
+        mins.push(INT_MAX);
+    }
+    
+    void push(int x) {
+        s.push(x);
+        if(mins.top() >= x)
+            mins.push(x);
+        else
+            mins.push(mins.top());
+    }
+    
+    void pop() {
+        s.pop();
+        mins.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int min() {
+        return mins.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->min();
+ */
