4b12c42201a722df6184f9fc8da89e549ed5a34a
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-03-15 23:06:00 +0800

    Update offer.cpp

--- c/offer.cpp
+++ c/offer.cpp
@@ -785,3 +785,22 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->min();
  */
+
+// 31
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> s;
+        int n = pushed.size();
+        int j = 0;
+        for(int i = 0; i < n; i++) {
+            s.push(pushed[i]);
+            while(! s.empty() && s.top() == popped[j]) {
+                s.pop();
+                j++;
+            }
+        }
+
+        return s.empty();
+    }
+};
