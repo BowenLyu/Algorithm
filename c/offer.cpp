8ec2d170ba785a6e53fd58c966f1dbef8d7a94bb
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-03-22 14:35:49 +0800

    Update offer.cpp

--- c/offer.cpp
+++ c/offer.cpp
@@ -1288,3 +1288,54 @@ public:
         return ans;
     }
 };
+
+// 41 
+// 双最小（大）堆实现
+class MedianFinder {
+public:
+    priority_queue<int, vector<int>, less<int> > q1;
+    priority_queue<int, vector<int>, greater<int> > q2;
+    /** initialize your data structure here. */
+    MedianFinder() {
+
+    }
+    
+    void addNum(int num) {
+        if(q1.size() > q2.size()) {
+            if(num < q1.top()) {
+                q2.push(q1.top());
+                q1.pop();
+                q1.push(num);
+            }
+            else
+                q2.push(num);
+
+        }
+        else {
+            if(!q2.empty() && num > q2.top()) {
+                q1.push(q2.top());
+                q2.pop();
+                q2.push(num);
+            }
+            else
+                q1.push(num);
+        }
+            
+    }
+    
+    double findMedian() {
+        if(q1.size() == 0)
+            return {};
+        if(q1.size() == q2.size())
+            return ((double)q1.top() + (double)q2.top())/2;
+        else
+            return (double)q1.top();
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
