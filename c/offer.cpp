810bd74369f569b5edab993c4a1f1ac1cf8dccab
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-03-30 14:45:23 +0800

    Update offer.cpp

--- c/offer.cpp
+++ c/offer.cpp
@@ -1652,3 +1652,25 @@ public:
             return r - l + 1;
     }
 };
+
+// 53
+// 还是讨厌的边界情况
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0;
+        int r = n-1;
+        while(l < r) {
+            int mid = l + (r-l)/2;
+            if(nums[mid] == mid)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        if(nums[l] == l)
+            return l+1;
+        else
+            return l;
+    }
+};
