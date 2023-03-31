48085169ede0e4df363db1806cecabbc808b7858
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-03-31 10:35:33 +0800

    Update offer.cpp

--- c/offer.cpp
+++ c/offer.cpp
@@ -1707,3 +1707,26 @@ public:
         dfs(root->left);
     }
 };
+
+// 55
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        
+        int ld = maxDepth(root->left);
+        int rd = maxDepth(root->right);
+
+        return max(ld,rd) + 1;
+    }
+};
