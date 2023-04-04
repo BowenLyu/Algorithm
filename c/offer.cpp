11f05147c505688167cb9e028dc3a8ab13258bd2
Bowen Lyu <31814870+BowenLyu@users.noreply.github.com>
2023-04-04 15:02:17 +0800

    Update offer.cpp

--- c/offer.cpp
+++ c/offer.cpp
@@ -1148,6 +1148,61 @@ public:
     }
 };
 
+// 36
+// 好难...
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+        left = NULL;
+        right = NULL;
+    }
+
+    Node(int _val, Node* _left, Node* _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+class Solution {
+public:
+    Node* head;
+    Node* pre;
+
+    Node* treeToDoublyList(Node* root) {
+        if(root == NULL)
+            return NULL;
+        dfs(root);
+        head->left = pre;
+        pre->right = head;
+        return head;
+    }
+
+    void dfs(Node* cur) {
+        if(cur == NULL)
+            return;
+        dfs(cur->left);
+        if(pre == NULL)
+            head = cur;
+        else 
+            pre->right = cur;
+        cur->left = pre;
+        pre = cur;
+        dfs(cur->right);
+        
+    }
+
+};
+
 // 37
 // 字符串还是不熟练，有些实现函数不知道，得找时间把string看一遍
 /**
